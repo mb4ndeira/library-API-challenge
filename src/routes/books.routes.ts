a251817162d3ee95e02a1f1c7e408b078db0a82b
mb4ndeira <mbandeira.cs@gmail.com>
2021-09-13 21:12:52 -0300

    feat: connect book listing use case to books routes

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 
 import { addBookController } from 'src/useCases/addBook'
+import { listBooksController } from 'src/useCases/listBooks'
 
 const booksRoutes = Router()
+
+booksRoutes.get('/', (request, response) => {
+  return listBooksController.handle(request, response)
+})
 
 booksRoutes.post('/', (request, response) => {
   return addBookController.handle(request, response)
