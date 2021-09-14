a17a7f7edae2cbcb1e67949b6fc2dc917198c7d7
mb4ndeira <mbandeira.cs@gmail.com>
2021-09-13 22:01:59 -0300

    feat: connect book deletion use case to books routes

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import { addBookController } from 'src/useCases/addBook'
 import { listBooksController } from 'src/useCases/listBooks'
+import { deleteBookController } from 'src/useCases/deleteBook'
 
 const booksRoutes = Router()
 
@@ -11,6 +12,10 @@ booksRoutes.get('/', (request, response) => {
 
 booksRoutes.post('/', (request, response) => {
   return addBookController.handle(request, response)
+})
+
+booksRoutes.delete('/', (request, response) => {
+  return deleteBookController.handle(request, response)
 })
 
 export { booksRoutes }
